f6646d3f039020fb123d0df4f9816c6ce6784c92
Goyo <gregory.vargas18@gmail.com>
2018-06-13 12:36:58 -0500

    added 5 entries to People table

--- sql_1_practice.sql
+++ sql_1_practice.sql
@@ -6,3 +6,18 @@ CREATE TABLE Person(
   City string,
   Favorite_Color string
 );
+
+INSERT INTO Person(Name, Age, Height, City, Favorite_Color)
+VALUES ('Goyo', 23, 180, 'Austin', 'Purple');
+
+INSERT INTO Person(Name, Age, Height, City, Favorite_Color)
+VALUES ('JoeD', 23, 182, 'Austin', 'Green');
+
+INSERT INTO Person(Name, Age, Height, City, Favorite_Color)
+VALUES ('Ted', 34, 200, 'Bryan', 'Blue');
+
+INSERT INTO Person(Name, Age, Height, City, Favorite_Color)
+VALUES ('Shelby', 21, 170, 'Provo', 'Orange');
+
+INSERT INTO Person(Name, Age, Height, City, Favorite_Color)
+VALUES ('Maggie', 22, 174, 'San Jose', 'Yellow');
