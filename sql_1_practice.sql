f937f72e45b0090f6e1f5268d6cc97671c234314
Goyo <gregory.vargas18@gmail.com>
2018-06-13 15:27:10 -0500

    completed Orders

--- sql_1_practice.sql
+++ sql_1_practice.sql
@@ -1,4 +1,5 @@
 /*** People ***/
+
 -- People 1.
 CREATE TABLE Person(
   ID INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -38,17 +39,46 @@ SELECT * FROM Person WHERE Age < 20 OR Age > 30;
 -- People 9.
 SELECT * FROM Person WHERE Age != 27;
 
--- People 10/
+-- People 10.
 SELECT * FROM Person WHERE Favorite_Color != 'red';
 
--- People 11/
+-- People 11.
 SELECT * FROM Person WHERE  Favorite_Color NOT LIKE 'red' AND Favorite_Color NOT LIKE 'blue';
 
--- People 12/
+-- People 12.
 SELECT * FROM Person WHERE Favorite_Color LIKE 'orange' OR Favorite_Color LIKE 'green';
 
--- People 13/
+-- People 13.
 SELECT * FROM Person WHERE Favorite_Color IN('Orange', 'Green', 'Blue');
 
--- People 14/
+-- People 14.
 SELECT * FROM Person WHERE Favorite_Color IN('Yellow', 'Purple');
+
+/*** Orders ***/
+
+-- Orders 1.
+CREATE TABLE Orders(
+  PersonID integer,
+  ProductName string,
+  ProductPrice float,
+  Quantity integer
+);
+
+-- Orders 2.
+INSERT INTO Orders(PersonID, ProductName, ProductPrice, Quantity)
+VALUES (0, 'chicken', 2.45, 1), (1, 'back scratcher', 8.69, 2), (0, 'spinach', 1.48, 3), (2, 'laptop', 1312.22, 1), (3, 'book', 5.70, 2);
+
+-- Orders 3.
+SELECT * FROM Orders;
+
+-- Orders 4.
+SELECT SUM(Quantity) FROM Orders;
+
+-- Orders 5.
+SELECT SUM(ProductPrice * Quantity) FROM Orders;
+
+-- Orders 6.
+SELECT SUM(ProductPrice * Quantity) FROM Orders WHERE PersonID = 0;
+SELECT SUM(ProductPrice * Quantity) FROM Orders WHERE PersonID = 1;
+SELECT SUM(ProductPrice * Quantity) FROM Orders WHERE PersonID = 2;
+
